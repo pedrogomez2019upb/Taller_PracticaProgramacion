2a1f468e510fb5c2590533296ed0ddd4119fc52a
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-04 13:42:43 -0500

    Se sube el ejercicio 3 con nombre de archivo 3.py funcionando y compilando.

---
+++ 3.py
@@ -0,0 +1,9 @@
+#Algoritmo que imprima el cuadrado de un número
+#Primero saludamos al cliente y le solicitamos el número que se va a llevar al cuadrado
+#Esta variable la vamos a poner como un float para que pueda ser cualquier valor
+x=float(input("Bienvenido! Por favor ingresa el número que quieres llevar al cuadrado: "))
+#Declaramos otra varialbe que tenga el procedimiento matemático que eleve la variable x al cuadrado
+y=x*x
+#Finalmente imprimimos la respuesta con la variable "x" y "y"
+print ("El número {} al cuadrado da como el resultado de: {}".format(x,y))
+#Desarrollado por Pedro Gómez / ID:000396221
