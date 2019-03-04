45b4d32e85c59fbac9287d1ee8d7238ea32b107a
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-04 11:28:14 -0500

    Se añade el primer punto funcional.

---
+++ 1.py
@@ -0,0 +1,7 @@
+#Unir dos variables en un texto
+#Vamos primero a recibir las dos variable a unir
+x=input("Bienvenido! Este programa te va a unir dos palabras. Por favor ingresa la primera palabra: ")
+y=input("Ahora ignresa la segunda palabra: ")
+#Finalmente vamos a imprimir las dos palabras mediante arreglandolas en un print
+print("{} {}".format(x,y))
+#Desarrollado por Pedro Gómez / ID:000396221
