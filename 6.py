ec9c2c9ce96af162711ef2ee10193464d89962b9
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-04 14:09:15 -0500

    Se sube el archivo 6.py del ejercicio #6 funcionando y compilando.

---
+++ 6.py
@@ -0,0 +1,12 @@
+#Algoritmo que haga la suma ,resta , multiplicación , división y residuo de dos números.
+#Primero vamos a solicitar los dos números a revisar.
+x=float(input("Bienvenido ! Por favor ingresa el primer valor: "))
+y=float(input("Ahora ingresa el segundo valor: "))
+#Ahora hacemos cuatro variables que hagan los respectivos procedimientos matemáticos.
+a=x+y
+b=x-y
+c=x*y
+d=x/y
+#Finalmente imprimimos los resultados con print y las variables.
+print("El resultado de la suma ,resta,multiplicación y división de {} y {} es respectivamente: {} {} {} {}".format(x,y,a,b,c,d))
+#Desarrollado por Pedro Gómez / ID:000396221
