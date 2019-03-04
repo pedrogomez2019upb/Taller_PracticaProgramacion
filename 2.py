af28bd0318f36138fc9b5984817c7c678b2aff65
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-04 13:36:15 -0500

    Se añade el punto número 2 del taller 2.py funcionando y compilando.

---
+++ 2.py
@@ -0,0 +1,6 @@
+#Algoritmo que imprima "Hola" y imprima un nombre X
+#Primero vamos a obtener el nombre a poner en el programa
+x=input("Bienvenido! Como te llamas?: ")
+#Finalmente imprimimos "Hola" con la variable x la cual será el nombre
+print("Hola {}!.".format(x))
+#Desarrollado por Pedro Gómez / ID:000396221
