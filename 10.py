436280109afc88261ec31820c850bd567fe53ccb
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-04 15:15:31 -0500

    Se sube el ejercicio 10 del taller en el archivo 10.py funcionando y compilando.

---
+++ 10.py
@@ -0,0 +1,9 @@
+#Area y Perimetro de un circulo.
+#Primero nos toca recibir el valor del radio de circulo a evaluar
+r=float(input("Bienvenido! Vamos a sacarle el área y perimetro de un circulo sabiendo que pi=3.1416 . Por favor ingresa el valor del radio: "))
+#Ahora se hace una variable que saque el area y perimetro del circulo.
+a=3.1416*(r*r)
+p=2*3.1416*r
+#Finalmente se imprime el resultado con un mensaje.
+print("El área de un círculo con radio {} es : {} , y el perímetro seria de: {}. Gracias por utilizar el programa.".format(r,a,p))
+#Desarrollado por Pedro Gómez / ID:000396221
