65f7a286db8c954d71425b413e943cd4a70f6189
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-04 13:58:49 -0500

    Se sube el ejercicio número 5 con el archivo 5.py funcionando y compilando.

---
+++ 5.py
@@ -0,0 +1,11 @@
+#Imprimir el resultado de cuatro operaciones matemáticas
+#Primero que todo damos un mensaje de bienvenida al usuario.
+print("Bienvenido! Vamos a imprimir el resultado de 4 procedimientos matemáticos. ")
+#Para ello vamos a hacer 4 variables que mantengan el procedimiento matemático
+a=-5+8*6
+b=(55+9)%9
+c=20+3*5/8
+d=5+15/3*2-8%3
+#Finalmente imprimimos los resultados
+print("Los resultados de los cinco procedimientos matemáticos son: {}  {}  {}  {} ".format(a,b,c,d))
+#Desarrollado por Pedro Gómez / ID:00396221
