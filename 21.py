b77afaf953d4a2e621a3db1aabc1b655646ea0f1
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-06 14:53:36 -0500

    Se añade el archivo 21.py funcionando y compilando.

---
+++ 21.py
@@ -0,0 +1,8 @@
+#Segundos a horas
+#Primero hay que recibir los segundos a convertir
+seg=int(input("Bienvenido! Vamos a convertir segundos a horas. Por favor ingresa la cantidad de segundos a convertir: "))
+#Se plantea la division para convertir los segundos a horas
+h=seg/3600
+#Finalmente se imprime el resultado
+print("El valor de segundos en horas es de: {} . Gracias por utilizar el programa.".format(h))
+#Desarrollado por Pedro Gómez / ID:000396221
