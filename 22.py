c47fbbf9b4400a8915550ef118fc1d24609a7d91
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-06 14:59:59 -0500

    Se añade el archivo 22.py funcionando y compilando.

---
+++ 22.py
@@ -0,0 +1,8 @@
+#Segundos a minutos
+#Primero vamos a recibir el valor de segundos a convertir
+seg=int(input("Bienvenido ! Vamos a convertir segundos a minutos. Por favor ingresa los segundos a convertir: "))
+#Planteamos la conversión a minutos
+min=seg/60
+#Finalmente imprimimos el valor
+print("Los segundos {} son iguales a {} minuto/s. Gracias por utilizar el programa.".format(seg,min))
+#Desarrollado por Pedro Gómez / ID:000396221
