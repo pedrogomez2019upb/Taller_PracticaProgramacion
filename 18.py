d02bec7964098ee9c906d526f14a91d3a8a68aa1
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-06 11:17:31 -0500

    Se añade el archivo 18.py funcionando y compilando.

---
+++ 18.py
@@ -0,0 +1,9 @@
+#Fuerza con la segunda ley de Newton
+#Para ello nos toca recolectar la masa y la aceleracion del objeto.
+m=float(input("Bienvenido! Vamos a calcular la fuerza de un objeto de acuerdo a la segunda ley de Newton . Por favor ingresa la masa del objeto: "))
+a=float(input("Ahora ingresa la aceleración de este mismo: "))
+#Planteamos la formula para sacar la fuerza
+f=m*a
+#Finalmente lo imprimimos
+print("El valor de la fuerza es: {}. Gracias por utilizar el programa.".format(f))
+#Desarrollado por Pedro Gómez / ID:000396221
