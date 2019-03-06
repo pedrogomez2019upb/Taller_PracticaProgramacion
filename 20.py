d075addba0a392f7503de4a8bc7142cb935a1e37
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-06 14:48:49 -0500

    Se añade el archivo 20.py funcionando y compilando.

---
+++ 20.py
@@ -0,0 +1,9 @@
+#Convierta un valor n en nn y nnn
+#Primero hay que recibir el valor n
+n=int(input("Bienvenido! Vamos a volver tu número un nn y nnn. Por favor ingresa el valor n: "))
+#Planteamos las ecuaciones que lo van a convertir
+nn=n*11
+nnn=n*111
+#Finalmente vamos a imprimir el valor creado
+print("Los valores convertidos son :  {}:{}:{} .".format(n,nn,nnn))
+#Desarrollado por Pedro Gómez / ID:000396221
