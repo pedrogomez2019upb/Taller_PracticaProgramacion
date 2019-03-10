f7f46b60fefc685658d3656dbfc0299816c862a0
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-10 16:46:13 -0500

    Se añade el archivo 26.py funcionando y compilando.

---
+++ 26.py
@@ -0,0 +1,13 @@
+#Algoritmo que revise condicionales true y false
+#Damos un mensaje de bienvenida
+print("Bienvenido! Vamos a evaluar diferentes operaciones de true y false.")
+#Ahora declaramos variables para los procedimientos booleanos
+a= True or False
+b= False or False
+c= True and True
+d= False and True
+e= (False and False) or (True and True)
+f=(False or False) and (True and True)
+#Finalmente se imprimen los resultados
+print("Los resultados de las expresiones booleanas son : {} {} {} {} {} {} .".format(a,b,c,d,e,f))
+#Desarrollado por Pedro Gómez / ID:000396221
