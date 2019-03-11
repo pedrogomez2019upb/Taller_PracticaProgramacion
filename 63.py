c184416f3e0c11b36c0d0c4a4c9cebf805d000d0
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-11 12:12:33 -0500

    Se crea el archivo 64.py

---
+++ 63.py
@@ -0,0 +1,16 @@
+#Escribe un algoritmo que lea 10 números y calcule su suma y su promedio
+n1=float(input("Bienvenido! Vamos a sacar el promedio de diez números. Por favor ingresa el primer número: "))
+n2=float(input("Ingresa el segundo número: "))
+n3=float(input("Ingresa el tercer número: "))
+n4=float(input("Ingresa el cuarto número: "))
+n5=float(input("Ingresa el quinto número: "))
+n6=float(input("Ingresa el sexto número: "))
+n7=float(input("Ingresa el séptimo número: "))
+n8=float(input("Ingresa el octavo número: "))
+n9=float(input("Ingresa el noveno número: "))
+n10=float(input("Ingresa el décimo número: "))
+#Varable para sacar el promedio
+prom=(n1+n2+n3+n4+n5+n6+n7+n8+n9+n10)/10
+#Imprimir el promedio
+print("El promedio de los números dados son: {} . Gracias por utilizar el programa.".format(prom))
+#Desarrollado por Pedro Gómez / ID:000396221
