7aae5b2690bb31317f97d6419de1f448cebf3711
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-11 11:05:53 -0500

    Se añade el archivo 59.py funcionando y compilando.

---
+++ 59.py
@@ -0,0 +1,11 @@
+#Escribe un algoritmo que imprima los n primeros números naturales
+#Hacemos un contador que empiece desde cero
+i=0
+#Mensaje de bienvenida para recibir los valores de n
+n=int(input("Bievenido! Vamos a imprimir los primeros n valores naturales. Ingresa el valor a revisar: "))
+#Hacemos una condicional for para este ejercico
+for i in range (0,n):
+    i=i+1
+    #Se imprime el resultado
+    print(i)
+#Desarrollado por Pedro Gómez / ID:000396221
