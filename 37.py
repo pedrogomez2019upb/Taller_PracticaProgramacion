bc886f85f6b6baf85fefc0cc2fe96b0c1ea2bd27
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-10 22:53:12 -0500

    Se sube el archivo 37.py funcionando y compilando.

--- 37.py
+++ 37.py
@@ -2,3 +2,14 @@
 #Primero hay que recibir los valores a analizar 
 distancia=float(input("Bienvenido! Vamos a calcular el precio de tu pasaje. Por favor ingrese la distancia a recorrer en Km: "))
 dias=float(input("Por favor ingrese el número de dias a viajar: "))
+#Vamos a calcular el precio del viaje 
+precio=5000*distancia
+#Vamos a poner las condicionales necesarias para que pueda sacar el valor del viaje
+if distancia>1000 and dias>7:
+    #Se aplica el descuento y se imprime
+    x=precio-(precio*0.15)
+    print("Felicitaciones! Se te va a hacer un descuento del 15% haciendo que tu viaje de ${} cueste ahora ${}. Gracias por viajar con nosotros.".format(precio,x))
+else:
+    #Se imprime el valor normal del viaje
+    print("El valor del viaje total es de: ${} . Gracias por viajar con nosotros.".format(precio))
+#Desarrollado por Pedro Gómez / ID:000396221
