c8c82445f765928552245354ea1b1ca6229c1ad3
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-11 10:43:08 -0500

    Se añade el archivo 56.py funcionando y compilando

---
+++ 56.py
@@ -0,0 +1,11 @@
+#Escribe un algoritmo que imprima los 10 primero números naturales
+#Hacemos un contador que empiece desde cero
+i=0
+#Mensaje de bienvenida
+print("Bienvenido! Vamos a imprimir los 10 primeros números naturales.")
+#Hacemos una condicional for para este ejercico
+for i in range(0,10):
+    i=i+1
+    #Se imprime el resultado
+    print(i)
+#Desarrollado por Pedro Gómez / ID:000396221
