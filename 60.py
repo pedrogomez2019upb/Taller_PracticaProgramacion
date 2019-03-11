20683635cf85b404d3a7af52f0fe6943e49af8ca
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-11 11:18:10 -0500

    Se agrega el archivo 60.py funcionando y compilando.

---
+++ 60.py
@@ -0,0 +1,10 @@
+#Escribe un algoritmo que dado un número n, imprima los números entre 1 y n siguiendo la siguiente secuencia:
+#1 -2 3 -4 5 -6 ….
+#Vamos a introducir el programa
+n=int(input("Bienvenido! Vamos a imprimir una secuencia dada. Ingrese el número a analizar: "))
+#Vamos a poner los valores necesarios en las condicionales.
+for i in range(1,n+1):
+	if i%2==0:
+		i=i*-1
+	print(i)
+
