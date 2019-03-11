782bb29ea5f2d5567d081ed1ac7d152f4e3b84cd
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-10 20:15:04 -0500

    Se sube avance del punto.

---
+++ 37.py
@@ -0,0 +1,4 @@
+#Escribe un algoritmo que determine el valor de un pasaje en avión, conociendo la distancia a recorrer, el número de días de estancia, y sabiendo que, si la distancia a recorrer es superior a 1000 Km y el número de días de estancia es superior a 7, la línea aérea le hace un descuento del 15%. (el precio por km. es de $5.000 aunque el mínimo a cobrar siempre es $100.000)
+#Primero hay que recibir los valores a analizar 
+distancia=float(input("Bienvenido! Vamos a calcular el precio de tu pasaje. Por favor ingrese la distancia a recorrer en Km: "))
+dias=float(input("Por favor ingrese el número de dias a viajar: "))
