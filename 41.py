d66502172e7133c30bd2fa0f426d49e8adbadea9
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-11 08:14:11 -0500

    Se agrega el archivo 41.py funcionando y compilando.

---
+++ 41.py
@@ -0,0 +1,34 @@
+#Imprimir el número en letras
+#Primero hay que solicitar el número
+a=int(input("Bienvenido! Por favor ingresa el número a convertir (0-10): "))
+#Hacemos condicionales if y then para que imprima el número
+if a==1:
+    print("El {} en letras es UNO. Gracias por utilizar el programa.".format(a))
+else:
+    if a==2:
+        print("El {} en letras es DOS. Gracias por utilizar el programa.".format(a))
+    else:
+        if a==3:
+            print("El {} en letras es TRES. Gracias por utilizar el programa.".format(a))
+        else:
+            if a==4:
+                print("El {} en letras es CUATRO. Gracias por utilizar el programa.".format(a))
+            else:
+                if a == 5:
+                    print("El {} en letras es CINCO. Gracias por utilizar el programa.".format(a))
+                else:
+                    if a == 6:
+                        print("El {} en letras es SEIS. Gracias por utilizar el programa.".format(a))
+                    else:
+                        if a == 7:
+                            print("El {} en letras es SIETE. Gracias por utilizar el programa.".format(a))
+                        else:
+                            if a==8:
+                                print("El {} en letras es OCHO. Gracias por utilizar el programa.".format(a))
+                            else:
+                                if a==9:
+                                    print("El {} en letras es NUEVE. Gracias por utilizar el programa.".format(a))
+                                else:
+                                    if a==10:
+                                        print("El {} en letras es DIEZ. Gracias por utilizar el programa.".format(a))
+#Desarrollado por Pedro Gómez / ID:000396221
