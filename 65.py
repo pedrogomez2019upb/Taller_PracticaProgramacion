b135dc23ca2c5022b3d89aac27ec8616999a4621
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-11 15:39:08 -0500

    Se añade adelanto del 65.py

---
+++ 65.py
@@ -0,0 +1,6 @@
+#Leer una cantidad variable de números e indicar el promedio de los números pares y el promedio de los números impares
+#Primero hay que recibir la cantidad de números a revisar
+n=int(input("Bienvenido! Vamos a sacar el promedio de números pares e impares. Por favor ingrese el número a analizar: "))
+#Ahora ponemos for para los números a analizar
+for i in range (0,n):
+    
