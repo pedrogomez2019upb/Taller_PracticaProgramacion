d720c763b1fc68b09efb967b21d943dfb957edd9
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-10 19:13:03 -0500

    Se añade el archivo 33.py funcionando y compilando.

---
+++ 33.py
@@ -0,0 +1,10 @@
+#Buscar el mayor de dos números
+#Se reciben los dos valores
+a=float(input("Bienvenido ! Vamos a ver cual número es mayor que otro. Ingresa el primer valor: "))
+b=float(input("Ingresa el segundo valor: "))
+#Ponemos condicional if y then
+if a>b:
+    print("El número {} es mayor que {} . Gracias por utilizar el programa.".format(a,b))
+else:
+    print("El número {} es mayor que {} . Gracias por utilizar el programa.".format(b,a))
+#Desarrollado por Pedro Gómez / ID:000396221
